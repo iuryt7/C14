4b63ecd933a9a5fa86cb3e045a1ffec6a45696f9
AlvaroCareli <122116260+alvarosamp@users.noreply.github.com>
2025-08-18 20:26:08 -0300

    Conflito

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ def main():
             print("-", pokemon["name"])
     else:
         print("Falha na requisição:", r.status_code)
+        
+def main():
+    r = requests.get("https://pokeapi.co/api/v2/pokemon")
+    if r.status_code == 200:
+        print("Requisição bem-sucedida!")
+        data = r.json()
+        print("Primeiros 10 pokémons:")
+        for pokemon in data.get("results", [])[:10]:
+            print("-", pokemon["name"])
+    else:
+        print("Falha na requisição:", r.status_code)
 
 if __name__ == "__main__":
     main()
